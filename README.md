91d9794cb0c5745d2d7f9fcf4437b5fe3fe71a0f
Dillon Welch <dillon.welch13@gmail.com>
2021-08-31 19:06:20 -0500

    Added a GUI. Updated README.md, game.js, and index.html. Added photos, sounds, fonts, and style.css.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # rock-paper-scissors
 A simple version of the classic game 'Rock, Paper, Scissors' that allows the user to play against a computer. 
+
+Skills Demonstrated: 
+
+1. How to create, add, and alter elements using DOM methods. 
+2. How to add CSS animations during certain events.  
+3. How to utilize active selectors to alter the styling of an element during an event. 
+4. How to utilize event listeners in JavaScript to perform certain tasks upon the occurence of said event. 
+5. How to add event listeners to a node list. 
+
+
--- game.js
+++ game.js
@@ -1,3 +1,59 @@
+const buttons = document.querySelectorAll("button");
+const parent = document.querySelector(".content");
+const results = document.createElement("div");
+const victorySound = document.querySelector("audio[class='victory-sound']");
+const losingSound = document.querySelector("audio[class='losing-sound'");
+
+results.setAttribute("style", "text-align: center; white-space: pre;" +
+        "padding-top: 20px; background-image: url('Pics/notebook.jpg');" +
+        "display: flex; flex: 1 0 80px; justify-content: center;" +
+        "margin: auto; width: 380px; border: 2px solid black; border-radius: 15px;" + 
+        "overflow: hidden;");
+results.textContent = "First to 5 points wins! Select a choice. \r\nRock, paper, or scissors.";
+
+
+let playerScore = 0; 
+let computerScore = 0;
+let isMatchOver = false;
+
+buttons.forEach((button) => {
+
+    button.addEventListener("click", () => {
+        if (button.className === "rock-btn") {
+            results.textContent = playRound("rock", computerPlay());
+        } else if (button.className === "paper-btn") {
+            results.textContent = playRound("paper", computerPlay());
+        } else if (button.className === "scissors-btn") {
+            results.textContent = playRound("scissors", computerPlay());
+        } 
+
+        if (!isMatchOver) {
+            let scoreStr = getScore(results.textContent) + "\r\n";
+            results.textContent += "\r\n" + scoreStr;
+            checkForWinner();
+        }
+
+        if (button.className === "reset-btn") {
+            resetGame();
+            victorySound.pause();
+            losingSound.pause();
+            victorySound.currentTime = 0;
+            losingSound.currentTime = 0;
+        }
+
+        if (isMatchOver && checkForWinner()) {
+            results.textContent = "You won the match! \r\n Do you want a rematch?";
+            victorySound.play();
+        } else if (isMatchOver && !checkForWinner()) {
+            results.textContent = "You lost the match. \r\n Do you want a rematch?";
+            losingSound.play();
+        }
+        
+    });
+  });
+
+parent.appendChild(results);
+
 function computerPlay() {
     let randomChoice = Math.floor(Math.random() * 3 + 1);
     
@@ -22,45 +78,33 @@ function playRound(playerSelection, computerSelection) {
         return "It's a tie!";
     } else {
         return("You won! " + playerSelection.substr(0, 1).toUpperCase()+playerSelection.substring(1).toLowerCase()
-                + " beats " + computerSelection + ".");
+                + " beats " + computerSelection.toLowerCase() + ".");
     }
 }
 
-function game() {
-    let computerScore = 0; 
-    let playerScore = 0;
-    let roundResult;
-    let computerChoice;
-    let playerSelection; 
-
-    for (let i = 0; i < 5; i++) {
-        computerChoice = computerPlay();
-        playerSelection = prompt("Choose rock, paper, scissors: ");
-        roundResult = playRound(playerSelection, computerChoice);
-        
-        if (roundResult.match("lose") !== null) {
-            computerScore++;
-        } else if (roundResult.match("tie") !== null) {
+function getScore(roundResult) {
+    if (roundResult.match("lose") !== null) {
+        computerScore++;
+    } else if (roundResult.match("tie") !== null) {
             
-        } else {
-            playerScore++;
-        }
-
-        console.log("(Round " + (i + 1) + ") Computer Score: " + computerScore + 
-                    "\t Player Score: " + playerScore);
-        console.log(roundResult);
-    }
-
-    if (playerScore === computerScore) {
-        return "This match is a tie!";
-    } else if (playerScore < computerScore) {
-        return "You lost the match! :(";
     } else {
-        return "You won the match! :)";
-    }
+        playerScore++;
+    } 
+    return "Computer: " + computerScore + "\t\t\t\tYou: " + playerScore;
 }
 
-console.log(game());
+function checkForWinner() {
+    let winner = false;
+    if (playerScore === 5 || computerScore === 5) {
+        winner = (playerScore > computerScore) ? true : false;
+        isMatchOver = true;
+    }
+    return winner;
+}
 
-
-
+function resetGame() {
+    playerScore = 0; 
+    computerScore = 0;
+    isMatchOver = false;
+    results.textContent = "First to 5 points wins! Select a choice. \r\nRock, paper, or scissors.";
+}
